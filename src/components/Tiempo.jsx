09cf9687156776d2d4409166a48c9fd66d059cf5
Miguel Astorga <mastorga.leiva@gmail.com>
2022-05-19 16:15:45 -0400

    404 img agregado

--- src/components/Tiempo.jsx
+++ src/components/Tiempo.jsx
@@ -5,21 +5,21 @@ export const Tiempo = ({ data }) => {
   const diaSlice = horario.slice(0, 10)
   const horaSlice = horario.slice(10, 16)
   return (
-    <div className='flex flex-col text-center'>
-      <h1 className='text-slate-800 text-5xl pt-3 sm:text-6xl sm:mx-4 sm:dnf install rust cargomy-1 sm:tracking-wider'>
+    <div className='flex flex-col text-center justify-center'>
+      <h1 className='text-slate-900 text-5xl pt-3 sm:text-6xl sm:mx-4 sm:dnf install rust cargomy-1 sm:tracking-wider'>
         {data.location.name}
       </h1>
       <div className='flex justify-center'>
-        <p className='text-slate-300 text-1xl mx-14 my-2 sm:text-3xl'>{`${diaSlice}`}</p>
-        <p className='text-slate-300 text-1xl mx-14 my-2 sm:text-3xl'>{`${horaSlice}`}</p>
+        <p className='text-slate-200 text-1xl mx-14 my-2 sm:text-3xl'>{`${diaSlice}`}</p>
+        <p className='text-slate-200 text-1xl mx-14 my-2 sm:text-3xl'>{`${horaSlice}`}</p>
       </div>
-      <p className='text-slate-300 text-1xl mx-4 my-2 sm:text-3xl '>
+      <p className='text-slate-200 text-1xl mx-4 my-2 sm:text-3xl '>
         {data.current.text}
       </p>
       <div>
-        <h1 className='text-slate-300 font-normal text-6xl mx-4 sm:text-5xl sm:font-semibold '>
+        <h1 className='text-slate-200 font-normal text-6xl mx-4 sm:text-5xl sm:font-semibold '>
           {data.current.temp_c}
-          <span className='text-slate-300 font-thin sm:font-normal'> C°</span>
+          <span className='text-slate-200 font-thin sm:font-normal'> C°</span>
         </h1>
       </div>
     </div>
--- src/components/TiempoMessage.jsx
+++ src/components/TiempoMessage.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
-
+import notFound from '/src/Imgs/SeekPng.com_donald-trump-toupee-png_3292234-min.png'
 export const TiempoMessage = ({ error }) => {
   return (
-    <div className='m-2 text-center text-black font-bold sm:text-lg md:text-2xl'>
+    <div className='text-2xl flex flex-col justify-center  items-center h-80 m-2 text-center font-bold md:text-4xl'>
       {error ? (
-        <h1>No se pudo encontrar la ciudad </h1>
+        <>
+          <h1 className='text-slate-200 mt-10'>
+            No se pudo encontrar la ciudad{' '}
+          </h1>
+          <div>
+            <img src={notFound} alt='' className='h-56 mt-10' />
+          </div>
+        </>
       ) : (
-        <h1>Ingrese una ciudad</h1>
+        <h1 className='text-slate-200 mt-10'>Ingrese una ciudad</h1>
       )}
     </div>
   )
